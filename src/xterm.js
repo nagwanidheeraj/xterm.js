f2137e36b7610bb13f060c2f7bbd7fbb78188045
Paris Kasidiaris <paris@sourcelair.com>
2016-08-26 15:54:10 +0300

    Make `src/xterm.js` ES2015 and export EventEmitter into its own file

--- src/xterm.js
+++ src/xterm.js
@@ -31,6 +31,8 @@
  *   other features.
  */
 
+import { EventEmitter } from './EventEmitter.js';
+
 (function (xterm) {
     if (typeof exports === 'object' && typeof module === 'object') {
         /*
@@ -46,8 +48,8 @@
         /*
          * Plain browser environment
          */
-        this.Xterm = xterm.call(this);
-        this.Terminal = this.Xterm; /* Backwards compatibility with term.js */
+        window.Xterm = xterm.call(window);
+        window.Terminal = window.Xterm; /* Backwards compatibility with term.js */
     }
 })(function() {
     /**
@@ -68,69 +70,6 @@
      */
 
     var window = this, document = this.document;
-
-    /**
-     * EventEmitter
-     */
-
-    function EventEmitter() {
-      this._events = this._events || {};
-    }
-
-    EventEmitter.prototype.addListener = function(type, listener) {
-      this._events[type] = this._events[type] || [];
-      this._events[type].push(listener);
-    };
-
-    EventEmitter.prototype.on = EventEmitter.prototype.addListener;
-
-    EventEmitter.prototype.removeListener = function(type, listener) {
-      if (!this._events[type]) return;
-
-      var obj = this._events[type]
-        , i = obj.length;
-
-      while (i--) {
-        if (obj[i] === listener || obj[i].listener === listener) {
-          obj.splice(i, 1);
-          return;
-        }
-      }
-    };
-
-    EventEmitter.prototype.off = EventEmitter.prototype.removeListener;
-
-    EventEmitter.prototype.removeAllListeners = function(type) {
-      if (this._events[type]) delete this._events[type];
-    };
-
-    EventEmitter.prototype.once = function(type, listener) {
-      var self = this;
-      function on() {
-        var args = Array.prototype.slice.call(arguments);
-        this.removeListener(type, on);
-        return listener.apply(this, args);
-      }
-      on.listener = listener;
-      return this.on(type, on);
-    };
-
-    EventEmitter.prototype.emit = function(type) {
-      if (!this._events[type]) return;
-
-      var args = Array.prototype.slice.call(arguments, 1)
-        , obj = this._events[type]
-        , l = obj.length
-        , i = 0;
-
-      for (; i < l; i++) {
-        obj[i].apply(this, args);
-      }
-    };
-
-    EventEmitter.prototype.listeners = function(type) {
-      return this._events[type] = this._events[type] || [];
-    };
 
 
     /**
